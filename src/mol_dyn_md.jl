5ae92ed6cf7cd59c03ae5d40699059de2407b4c7
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 14:31:15 -0700

    Other formulation
    Other formulation of velocity verlet.

--- src/mol_dyn_md.jl
+++ src/mol_dyn_md.jl
@@ -24,16 +24,31 @@ function stretch_gradient(a, b, k_ab, r_ab_eq)
 end
 
 # Propagate 1-2 bond stretch trajectories
-function stretch_velocity_verlet(qs, vs, accels, one_two_bonds, one_two_bonds_kab, one_two_bonds_req, ms, dt, num_steps)
-    for time_i in 2:num_steps
+# function stretch_velocity_verlet(qs, vs, accels, one_two_bonds, one_two_bonds_kab, one_two_bonds_req, ms, dt, num_steps)
+#     for time_i in 2:num_steps
+#         for bond_i in [1 2]
+#             k_ab = one_two_bonds_kab[bond_i]
+#             r_eq = one_two_bonds_req[bond_i]
+#             a_i = one_two_bonds[bond_i, 1]
+#             b_i = one_two_bonds[bond_i, 2]
+#             qs[time_i, a_i, :] = qs[time_i-1, a_i, :] + vs[time_i-1, a_i, :].*dt + accels[time_i-1, a_i, :].*(dt^2).*0.5
+#             accels[time_i, a_i, :] = -stretch_gradient(qs[time_i-1, a_i, :], qs[time_i-1, b_i, :], k_ab, r_eq) / ms[a_i]
+#             vs[time_i, a_i, :] = vs[time_i-1, a_i, :] + (accels[time_i-1, a_i, :] + accels[time_i, a_i, :]) .* dt .* 0.5
+#         end
+#     end
+# end
+
+function stretch_velocity_verlet(xs, vs, accels, one_two_bonds, one_two_bonds_kab, one_two_bonds_req, ms, dt, num_steps)
+    for time_i in 1:num_steps-1
         for bond_i in [1 2]
             k_ab = one_two_bonds_kab[bond_i]
             r_eq = one_two_bonds_req[bond_i]
-            a_i = one_two_bonds[bond_i, 1]
-            b_i = one_two_bonds[bond_i, 2]
-            qs[time_i, a_i, :] = qs[time_i-1, a_i, :] + vs[time_i-1, a_i, :].*dt + accels[time_i-1, a_i, :].*(dt^2).*0.5
-            accels[time_i, a_i, :] = -stretch_gradient(qs[time_i-1, a_i, :], qs[time_i-1, b_i, :], k_ab, r_eq) / ms[a_i]
-            vs[time_i, a_i, :] = vs[time_i-1, a_i, :] + (accels[time_i-1, a_i, :] + accels[time_i, a_i, :]) .* dt .* 0.5
+            atom_a = one_two_bonds[bond_i, 1]
+            atom_b = one_two_bonds[bond_i, 2]
+            v_mid = vs[time_i, atom_a, :] + 0.5 * accels[time_i, atom_a, :] * dt
+            xs[time_i+1, atom_a, :] = xs[time_i, atom_a, :] + v_mid * dt
+            accels[time_i+1, atom_a, :] = -stretch_gradient(xs[time_i, atom_a, :], xs[time_i, atom_b, :], k_ab, r_eq) / ms[atom_a]  # Should this be reduced mass???
+            vs[time_i+1, atom_a, :] = v_mid + 0.5 * accels[time_i+1, atom_a, :] * dt
         end
     end
 end
