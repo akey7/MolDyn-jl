7a30632e4057d286ab8193c82dcdd3994f801095
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 11:49:59 -0700

    Update hcl.jl

--- hcl.jl
+++ hcl.jl
@@ -28,19 +28,6 @@ accels = zeros(Float64, num_steps, 2, 3)
 # Masses: The masses of each atom (kg)
 ms = zeros(Float64, num_steps, 2, 1)
 
-# 1-2 Bonds
-# Rows are bonds, columns are atoms participating in bond
-# Note: This is specifying edges on a graph, so 1-2 also has 2-1
-one_two_bonds = [1 2; 2 1]
-
-# 1-2 Bonds, stretch constants
-# Note: There is a constant for each direction of the bond
-one_two_bonds_kab = [1.0 1.0]
-
-# 1-2 Bonds, equilibrium distances
-# Note: There is a distance for each direction of the bond
-one_two_bonds_req = [1.57e-10 1.57e-10]
-
 ###########################################################
 # INITIALIZE SIMULATION                                   #
 ###########################################################
@@ -51,11 +38,27 @@ r_ab_eq_hcl = 1.57e-10
 # Assume Cl is at 0,0,0 and H lies along the x-axis
 
 # HCl equilibrium bond length
-qs[1, 2, :] = [r_ab_eq_hcl, 0.0, 0.0]
+qs[1, 2, :] = [r_ab_eq_hcl*1.1, 0.0, 0.0]
 
 # Masses, Cl first then H
 ms[1] = 35 * kg_per_amu
 ms[2] = 1 * kg_per_amu
+
+# 1-2 Bonds
+# Rows are bonds, columns are atoms participating in bond
+# Note: This is specifying edges on a graph, so 1-2 also has 2-1
+
+one_two_bonds = [1 2; 2 1]
+
+# 1-2 Bonds, stretch constants
+# Note: There is a constant for each direction of the bond
+
+one_two_bonds_kab = [1.0 1.0]
+
+# 1-2 Bonds, equilibrium distances
+# Note: There is a distance for each direction of the bond
+
+one_two_bonds_req = [r_ab_eq_hcl r_ab_eq_hcl]
 
 ###########################################################
 # VELOCITY VERLET                                         #
