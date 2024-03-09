ceaa80c1a16e669a3f1b4205312e61da6e2450bc
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 07:53:21 -0700

    Update mol_dyn_md.jl

--- lib/mol_dyn_md.jl
+++ lib/mol_dyn_md.jl
@@ -1,13 +1,13 @@
 module MolDyn
 
-# Stretch energy
-function stretch(a::Vector{Float64}, b::Vector{Float64}, k_ab::Float64, r_ab_eq::Float64)
-    r_ab = sqrt(sum((a-b).^2))
-    0.5*k_ab*(r_ab-r_ab_eq)^2
+# Distance from atom a to b
+function r_ab(a::Vector{Float64}, b::Vector{Float64})
+    sqrt(sum((a-b).^2))
 end
 
-# First derivative of stretch energy
-function stretch_prime(a::Vector{Float64}, b::Vector{Float64}, k_ab::Float64, r_ab_eq::Float64)
+# Stretch energy
+function stretch(a::Vector{Float64}, b::Vector{Float64}, k_ab::Float64, r_ab_eq::Float64) 
+    0.5*k_ab*(r_ab(a,b)-r_ab_eq)^2
 end
 
 end
