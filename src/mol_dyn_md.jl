c12b5f65a8fa5ff3b1665e6d0dc98e351e1b80a7
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 11:59:12 -0700

    Update mol_dyn_md.jl

--- src/mol_dyn_md.jl
+++ src/mol_dyn_md.jl
@@ -13,16 +13,6 @@ function u_stretch(a::Matrix{Float64}, b::Matrix{Float64}, k_ab::Float64, r_ab_e
     0.5*k_ab*(r_ab(a,b)-r_ab_eq)^2
 end
 
-# The stretch gradient for a single bond. See eqns 2.33, 2.34, 2.25 in Cramer
-# function one_bond_stretch_gradient(a::Matrix{Float64}, b::Matrix{Float64}, k_ab::Float64, r_ab_eq::Float64)
-#     du_drab = k_ab*(r_ab(a, b)-r_ab_eq)
-#     drab_dxa = (a[1]-b[1])/r_ab(a, b)
-#     drab_dya = (a[2]-b[2])/r_ab(a, b)
-#     drab_dza = (a[3]-b[3])/r_ab(a, b)
-
-#     [drab_dxa, drab_dya, drab_dza] .* du_drab
-# end
-
 function one_bond_stretch_gradient(a, b, k_ab, r_ab_eq)
     du_drab = k_ab*(r_ab(a, b)-r_ab_eq)
     drab_dxa = (a[1]-b[1])/r_ab(a, b)
