93681f401a0adf30c536a037672db598f221975d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 08:28:55 -0700

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,23 @@
+using Test
+
+include("../src/mol_dyn_md.jl")
+using .MolDyn
+
+function setup_test_environment()
+    r_ab_eq_hcl = 1.57e-10
+    num_steps = 1000
+    qs = zeros(Float64, num_steps, 2, 3)
+    qs[1, 2, :] = [r_ab_eq_hcl, 0.0, 0.0]
+    return Dict(:qs => qs, :r_ab_eq_hcl => r_ab_eq_hcl)
+end
+
+using Test
+
+@testset "Example Test Set" begin
+    env = setup_test_environment()
+
+    @test begin
+        @test u_stretch(env[:qs][1,:,:], env[:qs][2,:,:], 1.0, env[:r_ab_eq_hcl]) == 0.0
+    end
+end
+
