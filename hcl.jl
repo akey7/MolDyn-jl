63c9ff40d5f0e50eed7d48af59142ec3be89fdbb
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-09 11:46:46 -0700

    Update hcl.jl

--- hcl.jl
+++ hcl.jl
@@ -10,10 +10,10 @@ using .MolDyn
 ###########################################################
 
 kg_per_amu = 1.661e-27
-num_steps = 1000
+num_steps = 100
 
 ###########################################################
-# ARRAYS HOLDING ATOM INFORMATION                         #
+# ARRAYS HOLDING ATOM AND BOND INFORMATION                #
 ###########################################################
 
 # Positions (qs), velocities (vs), and accelerations (accels) arrays:
@@ -27,6 +27,19 @@ accels = zeros(Float64, num_steps, 2, 3)
 
 # Masses: The masses of each atom (kg)
 ms = zeros(Float64, num_steps, 2, 1)
+
+# 1-2 Bonds
+# Rows are bonds, columns are atoms participating in bond
+# Note: This is specifying edges on a graph, so 1-2 also has 2-1
+one_two_bonds = [1 2; 2 1]
+
+# 1-2 Bonds, stretch constants
+# Note: There is a constant for each direction of the bond
+one_two_bonds_kab = [1.0 1.0]
+
+# 1-2 Bonds, equilibrium distances
+# Note: There is a distance for each direction of the bond
+one_two_bonds_req = [1.57e-10 1.57e-10]
 
 ###########################################################
 # INITIALIZE SIMULATION                                   #
@@ -45,22 +58,26 @@ ms[1] = 35 * kg_per_amu
 ms[2] = 1 * kg_per_amu
 
 ###########################################################
-# PROPAGATE ONE TIME STEP                                 #
+# VELOCITY VERLET                                         #
 ###########################################################
 
 dt = 1e-15
 
-qs[2,1,:] = qs[1,1,:] + vs[1,1,:].*dt + accels[1,1,:].*dt^2
-accels[2,1,:] = -one_bond_stretch_gradient(qs[1,1,:], qs[1,2,:], 1.0, r_ab_eq_hcl) / ms[1]
-vs[2,1,:] = vs[1,1,:] + (accels[1,1,:]+accels[2,1,:]).*dt.*0.5
-
-qs[2,2,:] = qs[1,2,:] + vs[1,2,:].*dt + accels[1,2,:].*dt^2
-accels[2,2,:] = -one_bond_stretch_gradient(qs[1,2,:], qs[1,1,:], 1.0, r_ab_eq_hcl) / ms[2]
-vs[2,2,:] = vs[1,2,:] + (accels[1,2,:]+accels[2,2,:]).*dt.*0.5
+for time_i in 2:num_steps
+    for bond_i in [1 2]
+        k_ab = one_two_bonds_kab[bond_i]
+        r_eq = one_two_bonds_req[bond_i]
+        a_i = one_two_bonds[bond_i, 1]
+        b_i = one_two_bonds[bond_i, 2]
+        qs[time_i, a_i, :] = qs[time_i-1, a_i, :] + vs[time_i-1, a_i, :] .* dt + accels[time_i-1, a_i, :].*dt^2
+        accels[time_i, a_i, :] = -one_bond_stretch_gradient(qs[time_i-1, a_i, :], qs[time_i-1, b_i, :], k_ab, r_eq) / ms[a_i]
+        vs[time_i, a_i, :] = vs[time_i-1, a_i, :] + (accels[time_i-1, a_i, :] + accels[time_i, a_i, :]) .* dt .* 0.5
+    end
+end
 
 ###########################################################
 # PRINT RESULT                                            #
 ###########################################################
 
-println("Cl start $(qs[1,1,:]), Cl end $(qs[2,1,:])")
-println("H start $(qs[1,2,:]), H end $(qs[2,2,:])")
+println("Cl start $(qs[1,1,:]), Cl end $(qs[100,1,:])")
+println("H start $(qs[1,2,:]), H end $(qs[100,2,:])")
